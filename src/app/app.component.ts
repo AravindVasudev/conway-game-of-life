4d4bdd6e9a6db4eefff1f25d279395280fc27d08
AravindVasudev <aravindv96@yahoo.com>
2018-10-22 03:36:25 +0530

    Added functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,13 @@ import { State } from './State';
 export class AppComponent {
   State: typeof State = State;
   board: State[][];
-  dimension = 50;
+  dimension = 15;
   isRunning = false;
-  speed = 0;
+  speed = 5;
   interval: any;
+  directions = [[-1, -1], [-1,  0], [-1, 1],
+                [ 0, -1],           [ 0, 1],
+                [ 1, -1], [ 1,  0], [1,  1]];
 
   constructor() {
     // Init board
@@ -30,8 +33,8 @@ export class AppComponent {
   }
 
   // Sets the given position to State.Active
-  setCell(i: number, j: number): void {
-    this.board[i][j] = State.Active;
+  toggleCell(i: number, j: number): void {
+    this.board[i][j] = this.board[i][j] === State.Active ? State.Empty : State.Active;
   }
 
   // Updates board dimension
@@ -42,17 +45,57 @@ export class AppComponent {
     this.board = this.constructBoard(this.dimension, State.Empty);
   }
 
+  // Toggles board's running state
   toggleRunning(): void {
     if (this.isRunning) {
       this.isRunning = false;
       clearInterval(this.interval);
     } else {
       this.isRunning = true;
-      this.interval = setInterval(() => this.next(), this.speed);
+      this.interval = setInterval(() => {
+        this.board = this.next(this.board);
+      }, this.speed);
     }
   }
 
-  next(): void {
-    console.log('NEXT');
+  /**
+   * Takes a board and returns board for next generation (Pure function)
+   * Rules:
+   * 1. Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+   * 2. Any live cell with two or three live neighbors lives on to the next generation.
+   * 3. Any live cell with more than three live neighbors dies, as if by overpopulation.
+   * 4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+   * [Refer https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules]
+   * 
+   * @param board Input board
+   */
+  next(board: State[][]): State[][] {
+    let newBoard = this.constructBoard(board.length, State.Empty);
+
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board.length; j++) {
+        let neighbourCount = 0;
+        this.directions.forEach(direction => {
+          let curI = i + direction[0];
+          let curJ = j + direction[1];
+
+          if (curI < 0 ||  curI >= board.length || curJ < 0 || curJ >= board.length) {
+            return;
+          }
+
+          board[curI][curJ] === State.Active && neighbourCount++;
+        });
+
+        if (board[i][j] === State.Active && (neighbourCount === 2 || neighbourCount === 3)) {
+          newBoard[i][j] = State.Active;
+        }
+
+        if (board[i][j] === State.Empty && neighbourCount === 3) {
+          newBoard[i][j] = State.Active;
+        }
+      }
+    }
+
+    return newBoard;
   }
 }
